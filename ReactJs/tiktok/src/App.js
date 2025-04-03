2467aaa99a1bbe0cc20ad65e05de440035e5102d
Tika299 <vukofa9120@gmail.com>
2025-04-03 21:58:21 +0700

    Update App.js

--- ReactJs/tiktok/src/App.js
+++ ReactJs/tiktok/src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+
+const oder = [100,200,300]
 
 function App() {
+  //bạn có thể truyền tham số khởi tạo cứng như này
+  //const [counter,setCounter] = useState(1);
+  //Truyền theo dạng callback sẽ nhận giá trị trả về làm tham số khởi tạo
+  const [counter,setCounter] = useState(() => {
+    const total = oder.reduce((total,curl)=>total+curl)
+    console.log(total)
+    return total
+  });
+
+  function handelCounter() {
+    //Bạn có thể dụng như này cách phổ thông
+    setCounter(counter+1)
+    //Cách dùng callback, cách này giúp set lại biến counter trong khi thực hiện
+    //setCounter(prev => prev +1)
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App' style={{padding:20}}>
+      <h1>{counter}</h1>
+      <button onClick={handelCounter}>Click</button>
     </div>
   );
 }
