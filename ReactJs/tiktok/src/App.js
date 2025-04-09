74efbca48ccd26a99f94580f5a5131842d44c987
Tika299 <vukofa9120@gmail.com>
2025-04-09 21:27:08 +0700

    commit exe today

--- ReactJs/tiktok/src/App.js
+++ ReactJs/tiktok/src/App.js
@@ -2,14 +2,13 @@ import { useState } from 'react';
 import Content from './Content';
 
 
+
 function App() {
-  const [show,setShow] = useState(false)
 
 
   return (
     <div style={{padding:20}}>
-      <button onClick={() => setShow(!show)}>Toggle</button>
-      {show && <Content/>}
+      {<Content/>}
     </div>
   )
 }
--- ReactJs/tiktok/src/Content.js
+++ ReactJs/tiktok/src/Content.js
@@ -1,9 +1,43 @@
- import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
- function Content() {
+const tabs = ['posts', 'comments', 'albums']
+
+function Content() {
+
+    const [posts, setPosts] = useState([]);
+    const [type, setType] = useState('posts');
+
+    useEffect(() => {
+        fetch(`https://jsonplaceholder.typicode.com/${type}`)
+            .then(res => res.json())
+            .then(posts => {
+                setPosts(posts);
+            })
+    },[type])
+
     return (
-        <h1>Hi anh em</h1>
-    )
- }
+        <div>
 
- export default Content;
+            {tabs.map(tab => {
+                return <button key={tab} style={type === tab ? {
+                    color: '#fff',
+                    backgroundColor: '#333'
+                } : {} } onClick={() => setType(tab)}>
+                    {tab}
+                </button>
+            })}
+
+            <h1>Hi anh em</h1>
+            
+
+
+            <ul>
+                {posts.map(post => {
+                    return <li key={post.id}>{post.title || post.name}</li>
+                })}
+            </ul>
+        </div>
+    )
+}
+
+export default Content;
