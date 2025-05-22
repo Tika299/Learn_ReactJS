37525c6faf396d59d6de4115a9b0cd54e9633c3d
Tika299 <vukofa9120@gmail.com>
2025-05-22 19:05:36 +0700

    commit learn useMemo

--- ReactJs/tiktok/src/App.js
+++ ReactJs/tiktok/src/App.js
@@ -3,6 +3,12 @@ import Content from './Content';
 import FakeChat from './FakeChat';
 
 
+//useMemo là một hook giúp tối ưu hóa hiệu suất của ứng dụng React bằng cách ghi nhớ giá trị đã tính toán trước đó và chỉ tính toán lại khi các phụ thuộc thay đổi.
+// Điều này giúp giảm thiểu việc tính toán lại các giá trị không cần thiết, từ đó cải thiện hiệu suất của ứng dụng.
+// useMemo nhận vào hai tham số: một hàm callback và một mảng phụ thuộc.
+// Hàm callback sẽ trả về giá trị cần ghi nhớ, còn mảng phụ thuộc sẽ xác định khi nào giá trị đó cần được tính toán lại.
+// Nếu mảng phụ thuộc không thay đổi, useMemo sẽ trả về giá trị đã ghi nhớ trước đó.
+
 
 function App() {
   const [showChat, setShowChat] = useState(false);
